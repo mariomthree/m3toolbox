128c4919f3b2bf20c10070565a3db6dd36e059a6
mariomtree <mario99manuel@gmail.com>
2020-02-18 07:42:15 +0200

    Remove facebook page and lastest twitters

--- m3-toolbox.php
+++ m3-toolbox.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: M3 Toolbox
  * Plugin URI:  https://github.com/mariomthree/M3Toolbox
- * Description: O M3 Toolbox é um conjunto de ferramentas e widgets que o tema webnews necessita para o seu funcionamento.
+ * Description: The M3 Toolbox is not a plugin, it is a set of tools and widgets that the webnews theme needs for its operation.
  * Author:      Mario Manuel Mabande
  * Author URI:  https://github.com/mariomthree/
  * Version:     1.0.0
@@ -13,7 +13,3 @@ $file_dir = dirname( __FILE__ );
 require $file_dir . '/widgets/ContactInfo.php';
 
 require $file_dir . '/widgets/SocialLinks.php';
-
-require $file_dir . '/widgets/FacebookPage.php';
-
-require $file_dir . '/widgets/LatestTweets.php';
